c4101a69cdead652f116e77df3d489a58a347ad4
Bini Barazany <bgt636@gmail.com>
2021-02-28 19:02:31 +0200

    Init commit

---
+++ index.ts
@@ -0,0 +1,41 @@
+import rl from "readline";
+const readline = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+const getInput = (text: string): Promise<string> => {
+    return new Promise<string>((resolve) => {
+        readline.question(text + "\n", name => {
+            resolve(name)
+        });
+    });
+}
+
+async function getFileContentByArgs() {
+    return "2,5,=4*{1}";
+    // const filename = process.argv[2];
+    // if (!Boolean(filename) && typeof filename !== "string") {
+    //     process.exit(1);
+    // }
+    // try {
+    //     return readFileSync(filename);
+    // } catch (error) {
+    //     console.error(error);
+    //     process.exit(1);
+    // }
+}
+
+async function main() {
+    const formula = await getFileContentByArgs();
+    while (true) {
+        const input = await getInput(`
+            choose what to do?
+            a) print the formula
+            b) change some cells
+        `);
+    }
+}
+
+main();
